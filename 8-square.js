e2dc93186bb170f9d9c01367358a15ee6858cc7a
Zineb Salhi <ziineb.salhi@gmail.com>
2025-07-13 21:52:20 +0100

    Add script to print a square of X based on argument size

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const size = Number(process.argv[2]);
+
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+} else {
+  console.log("Missing size");
+}
